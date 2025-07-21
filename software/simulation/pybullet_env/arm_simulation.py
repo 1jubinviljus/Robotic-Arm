73d645edaae0183c808a0afa06d91ce13307bab3
JustinJubinville <justinjubinville0@gmail.com>
2025-07-21 17:03:06 -0400

    simulation setup

---
+++ software/simulation/pybullet_env/arm_simulation.py
@@ -0,0 +1,22 @@
+import numpy as np
+import pybullet as p
+import pybullet_data
+import time
+
+p.connect(p.GUI)
+p.resetSimulation()
+p.setAdditionalSearchPath(pybullet_data.getDataPath())
+p.setGravity(0, 0, -9.81)
+p.setRealTimeSimulation(0)
+
+# load assets
+p.loadURDF("plane.urdf", [0, 0, 0], [0, 0, 0, 1])
+targid = p.loadURDF("software/simulation/urdf/robotic_arm.urdf", [0, 0, 0], [0, 0, 0, 1], useFixedBase=True)
+obj_of_focus = targid
+
+# Instead of time.sleep(10), run a loop:
+for _ in range(240*10):  # roughly 10 seconds at 240 Hz
+    p.stepSimulation()
+    time.sleep(1./240.)
+
+p.disconnect()
