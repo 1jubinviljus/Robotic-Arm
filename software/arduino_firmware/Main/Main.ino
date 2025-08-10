acfa29be11d9fa04902fc841d8b25fe5bbab09c4
JustinJubinville <justinjubinville0@gmail.com>
2025-08-09 20:01:43 -0400

    basic movement

--- software/arduino_firmware/Main/Main.ino
+++ software/arduino_firmware/Main/Main.ino
@@ -11,11 +11,5 @@ void setup() {
 }
 
 void loop() {
-  for (int i = 0; i < 200; i++) {
-    digitalWrite(STEP_PIN, HIGH);
-    delayMicroseconds(500);
-    digitalWrite(STEP_PIN, LOW);
-    delayMicroseconds(500);
-  }
-  delay(1000);
+  moveSteppers();
 }
--- software/arduino_firmware/Main/stepper_control.cpp
+++ software/arduino_firmware/Main/stepper_control.cpp
@@ -1,4 +1,14 @@
 #include "Arduino.h"
 #include "stepper_control.h"
+#include "pin_setup.h"
 
+void moveSteppers(){
+    for (int i = 0; i < 200; i++) {
+        digitalWrite(BASE_STEP_PIN, HIGH);
+        delayMicroseconds(500);
+        digitalWrite(BASE_STEP_PIN, LOW);
+        delayMicroseconds(500);
+    }
+    delay(1000);
+}
 
